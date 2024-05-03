a9b9d4dcbc396009c59edccb012e08d82c8caa0f
Melissa <programadorimd@hotmail.com>
2024-05-03 14:50:09 -0300

    adicionando accordion no faq

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-const tabImg = document.querySelectorAll('.js-tabimg li');
+function activeTabInit() { 
+    const tabImg = document.querySelectorAll('.js-tabimg li');
 
-const tabContent = document.querySelectorAll('.js-tabcontent section');
-tabContent[0].classList.add('ativo')
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    tabContent[0].classList.add('ativo')
 
-function activeTab(){
+    function activeTab(){
 
-    tabContent.forEach((tab)=>{
-        tab.classList.remove('ativo')
+        tabContent.forEach((tab)=>{
+            tab.classList.remove('ativo')
+        })
+        const section = '.'+ this.dataset.img
+    
+        const select = document.querySelector(section)
+
+        select.classList.add('ativo')
+
+    }
+
+    tabImg.forEach((img)=>{
+        img.addEventListener('click', activeTab)
     })
-    const section = '.'+ this.dataset.img
-   
-    const select = document.querySelector(section)
-
-    select.classList.add('ativo')
-
 }
 
-tabImg.forEach((img)=>{
-    img.addEventListener('click', activeTab)
-})
+function accordionListInit(){
+    const accordionList = document.querySelectorAll('#faq dt')
+
+    function activeAccordion(){
+        this.classList.toggle('ativo');
+        this.nextElementSibling.classList.toggle('ativo')
+    }
+
+    accordionList.forEach((accordion)=>{
+        accordion.addEventListener('click', activeAccordion)
+    })
+}
+
+function init(){
+    activeTabInit();
+    accordionListInit();
+}
+
+init()
