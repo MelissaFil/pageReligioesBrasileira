f3a5755cf3b6cabec78415bb937baa592939e9f0
Melissa <programadorimd@hotmail.com>
2024-05-03 13:23:26 -0300

    function active tab pra mostrar descrição de acordo com a imagem selecionada

---
+++ script.js
@@ -0,0 +1,20 @@
+const tabImg = document.querySelectorAll('.js-tabimg li');
+
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+function activeTab(){
+
+    tabContent.forEach((tab)=>{
+        tab.classList.remove('ativo')
+    })
+    const section = '.'+ this.dataset.img
+   
+    const select = document.querySelector(section)
+
+    select.classList.add('ativo')
+
+}
+
+tabImg.forEach((img)=>{
+    img.addEventListener('click', activeTab)
+})
