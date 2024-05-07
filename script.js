30b8e32070b365abd70c45abd6a58f4d9857023d
Melissa <programadorimd@hotmail.com>
2024-05-07 12:35:53 -0300

    section de dados populacionais

--- script.js
+++ script.js
@@ -34,10 +34,44 @@ function accordionListInit(){
         accordion.addEventListener('click', activeAccordion)
     })
 }
+function atualizarPorcentagem(i, max, item, tempoAtraso){
+    if (i <= max) {
+      setTimeout(function() {
+          item.nextElementSibling.textContent = i + "%";
+          atualizarPorcentagem(i + 1, max, item, tempoAtraso);
+      }, tempoAtraso);
+  }
+}
+function contagemPorcentagemIniti(){
+    const dtReligioes = document.querySelectorAll(".porcentagem-religioes dt")
+
+    dtReligioes.forEach((item)=>{
+        switch(item.dataset.religiao){
+            case 'catolico':
+                atualizarPorcentagem(0, 49, item, 40);    
+            case 'evangelico':
+                atualizarPorcentagem(0, 26, item, 60); 
+            case 'espirita':
+                atualizarPorcentagem(0, 4, item, 200);  
+            case 'umbanda':
+                atualizarPorcentagem(0, 2, item, 250);  
+            
+        }
+    })
+      
+}
 
 function init(){
     activeTabInit();
     accordionListInit();
+
+    window.addEventListener("scroll", function() {
+        let sectionDados = document.getElementById("dados").getBoundingClientRect();
+        if (sectionDados.bottom <= window.innerHeight) {
+            contagemPorcentagemIniti();
+            window.removeEventListener("scroll", arguments.callee);
+        }
+    });
 }
 
 init()
